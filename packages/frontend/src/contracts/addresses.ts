39a66edccdbc584c990ebb9a05d7e1262af6988c
Osfoce <oselufortunatus2002@gmail.com>
2026-01-21 12:34:48 +0100

    fullstack foundation

---
+++ packages/frontend/src/contracts/addresses.ts
@@ -0,0 +1,4 @@
+export const MINI_LEND_ADDRESS = '0x...'
+export const BORROW_TOKEN = '0x...'
+export const ETH_ADDRESS =
+  '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
---
+++ packages/frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { WagmiProvider } from 'wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { wagmiConfig } from './wagmi'
+import App from './App'
+import './index.css'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <WagmiProvider config={wagmiConfig}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </WagmiProvider>
+)
